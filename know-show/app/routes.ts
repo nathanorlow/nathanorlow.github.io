29f7f81f84ff576bda996cb1c1a58f3ae6c736d0
nathanorlow <nathan.orlow@gmail.com>
2025-09-23 20:11:10 -0400

    Updates

--- know-show/app/routes.ts
+++
@@ -1,8 +0,0 @@
-import { type RouteConfig, index, route } from "@react-router/dev/routes";
-
-export default [
-    index("routes/home.tsx"),
-    route("create/:encodedString", "routes/create.tsx"),
-    route("solve/:encodedString", "routes/solve.tsx"),
-
-] satisfies RouteConfig;
